627c2186096f20ace7bc24b3a76b7ad10f54fec3
Elly <elly@Elly-MacBook-Pro.local>
2021-12-11 10:22:11 +0700

    Script Builder FE (in progress)

---
+++ frontend/src/data/navbar.js
@@ -0,0 +1,23 @@
+
+export const links = [
+    {
+        id: 1,
+        url: '/',
+        text: 'Home',
+    },
+    {
+        id: 2,
+        url: '/discover',
+        text: 'Discover',
+    },
+    {
+        id: 3,
+        url: '/list',
+        text: 'Your List',
+    },
+    {
+        id: 4,
+        url: '/builder',
+        text: 'Script Builder',
+    }
+]
---
+++ frontend/src/screens/ScriptBuilder/ScriptBuilder.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import NavBar from '../../components/NavBar'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
+
+const ScriptBuilder = () => {
+
+    const onCreateScript = async () => {
+        return 0;
+    }
+
+    return (
+        <div className='container'>
+            <NavBar/>
+            <section className='script-container'>
+                <div className='script-center'>
+                    <h2>Script Builder</h2>
+                    <button type="button" 
+                    title="ScriptBuilder"
+                    onClick={onCreateScript}>
+                        <div className='content'>
+                            <span className='title'>Create new script</span>
+                            <span className='description'>Make your own story</span>
+                        </div>
+                        <FontAwesomeIcon icon={faPlus} className='plus'/>
+                    </button>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default ScriptBuilder
