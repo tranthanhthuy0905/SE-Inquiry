4802768ab7b2c6f437a993c0f00a55ebb82032ea
Elly <elly@Elly-MacBook-Pro.local>
2022-01-02 11:27:51 +0700

    Update backend + frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,18 @@ import CreatePage from './screens/CreatePage/CreatePage';
 import ScriptBuilder from "./screens/ScriptBuilder/ScriptBuilder";
 import ScriptCreate from "./screens/ScriptCreate/ScriptCreate";
 import Connection from './screens/Connection/Connection';
+import IndvPage from './screens/IndvPage/IndvPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/main-menu" element={<Menu/>}/>
+        <Route path="/" element={<Menu/>}/>
         <Route path="/create-page" element={<CreatePage/>}/>
         <Route path="/script-building" element={<ScriptBuilder />}/>
         <Route path="/script-creating" element={<ScriptCreate/>}/>
         <Route path="/connections" element={<Connection/>}/>
+        <Route path="/individual" element={<IndvPage/>}/>
       </Routes>
     </Router>
   );
