e83d4dce639346f0457a99d39481af1488fedfc3
Elly <elly@192.168.2.13>
2022-01-13 11:12:08 +0700

    Update Preview Page calling BE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,12 @@ import ScriptBuilder from "./screens/ScriptBuilder/ScriptBuilder";
 import ScriptCreate from "./screens/ScriptCreate/ScriptCreate";
 import Connection from './screens/Connection/Connection';
 import PreviewPage from './screens/Preview/PreviewPage';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return ( 
+    <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element={<Menu/>}/>
@@ -15,10 +18,13 @@ function App() {
           <Route path="/script-building" element={<ScriptBuilder />}/>
           <Route path="/script-creating" element={<ScriptCreate/>}/>
           <Route path="/connections" element={<Connection/>}/>
-          
           <Route path="/preview" element={<PreviewPage/>}/>
+          
         </Routes>
       </Router>
+    </Provider>
   );
 }
 export default App;
+//<Provider store={store}>
+//<Route path="/preview" element={<PreviewPage/>}/>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Provider} from 'react-redux';
-import store from './redux/store';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
